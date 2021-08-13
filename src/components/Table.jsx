62ebb73b5f808a1e56f1ea4b3f09631b939bac80
Peter Lebedev <LPeter@mail.ru>
2021-08-13 14:00:17 +0300

    add simple states for sort and filter

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSortUp } from '@fortawesome/free-solid-svg-icons'
 import './table.css'
@@ -11,20 +11,36 @@ export default function Table({ data, columns, headers }) {
     columns = Object.keys(data[0]);
   }
   if (!Array.isArray(headers)) {
-    headers = Object.keys(data[0]);
+    headers = columns;
+  }
+  // state for active dataset to show in table
+  const [currentData, setSurrentData] = useState(data);
+  // state for sorting
+  const [sortBy, setSortBy] = useState({ by: "id", ascending: true });
+  //state for filter
+  const [filter, setFilter] = useState("");
+  // main handler
+  const clickHandler = (event) => {
+    const target = event.target;
+    const parent = target.parentNode;
+    console.log(target, parent);
   }
 
   return (
-    <table className="table table-hover table-bordered">
+    <table className="table table-hover table-bordered" onClick={clickHandler}>
       <thead>
         <tr>
           {
-            headers.map((h) => <th scope="col" key={h}>{h} <FontAwesomeIcon icon={faSortUp} /></th>)
+            headers.map((h, i) =>
+              <th
+                scope="col"
+                key={h}>{h}
+                <FontAwesomeIcon icon={faSortUp} /></th>)
           }
         </tr>
       </thead>
       <tbody>
-        {data.map((item, i) =>
+        {currentData.map((item, i) =>
           <tr key={i}>
             {columns.map((col, j) => <td key={i + "_" + j}>{item[col]}</td>)}
           </tr>
