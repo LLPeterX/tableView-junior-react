ed74674d0ce86b6e55cd7598e6f0315cb3b777b1
Peter Lebedev <LPeter@mail.ru>
2021-08-16 11:59:27 +0300

    change volume - done

--- src/App.js
+++ src/App.js
@@ -9,24 +9,30 @@ import Options from "./components/Options";
 
 
 function App() {
+  const volumes = [5, 10];
   //state to hold received data
   const [people, setPeople] = useState([]);
   // state to switch URL. default 50 (50 - small; 1000 - big)
-  const [volume, setVolume] = useState(50);
+  const [volume, setVolume] = useState(volumes[0]);
   // state for loading
   const [isLoading, setIsLoading] = useState(true);
 
   // fetch data from www.filltext.com
   useEffect(() => {
-    setIsLoading(false);
-    console.log(' App:useEffect volume=', volume);
-    const baseURL = `http://www.filltext.com/?rows=${volume}&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
-    axios.get(baseURL)
-      .then(response => {
-        console.log('after fetch: size=', response.data.length);
-        setPeople(response.data);
-        setIsLoading(false);
-      })
+    async function fetchData() {
+      console.log(' App:useEffect volume=', volume);
+      const baseURL = `http://www.filltext.com/?rows=${volume}&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`;
+      const response = await fetch(baseURL);
+      if (response.ok) {
+        let data = await response.json();
+        console.log(`received ${data.length} records`, data);
+        setPeople(data);
+      }
+      setIsLoading(false);
+    }
+    setIsLoading(true);
+    fetchData();
+
   }, [volume]);
 
   const handleChangeVolume = (vol) => {
@@ -37,7 +43,7 @@ function App() {
 
   return (
     <div className="container">
-      <Options volume={volume} setVolume={handleChangeVolume} />
+      <Options volumes={volumes} volume={volume} setVolume={handleChangeVolume} />
       {isLoading ?
         <Loader
           className="centered"
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-function Options({ volume, setVolume }) {
+function Options({ volumes, volume, setVolume }) {
 
   const [currentVolume, setcurrentVolume] = useState(volume);
 
@@ -9,26 +9,40 @@ function Options({ volume, setVolume }) {
     setcurrentVolume(+e.target.value);
     setVolume(+e.target.value);
   }
-  //console.log(`  volume=${volume}, curV=${currentVolume} types=${typeof volume}/${typeof currentVolume}`);
   return (
-    <div className="row justify-content-between mb-3">
-      {/* <form> */}
+    <div className="row justify-content-between">
+      <div>Объем данных</div>
       <div className="btn-group col-2" role="group" aria-label="Select data volume">
-        <input type="radio" className="btn-check" name="vol" id="vol50" checked={currentVolume === 50} value="50"
-          onClick={handleChangeVolume}
-        />
-        <label className="btn btn-outline-primary" htmlFor="vol50">50</label>
+        {
+          volumes.map(v =>
+            <div className="form-check pe-4" key={v}>
+              <input className="form-check-input" type="radio" name="vol" id={"vol" + v} checked={currentVolume === v} value={v}
+                onChange={handleChangeVolume}
 
-        <input type="radio" className="radio btn-check" name="vol" id="vol1000" checked={currentVolume === 1000} value="1000"
-          onClick={handleChangeVolume}
-        />
-        <label className="btn btn-outline-primary" htmlFor="vol1000">1000</label>
+              />
+              <label className="form-check-label" htmlFor={"vol" + v}>{v}</label>
+            </div>
+          )
+        }
       </div>
+      {/* <input type="radio" className="btn-check" name="vol" id={"vol" + v} checked={currentVolume === v} value={v} 
+          //   // onClick={handleChangeVolume}
+          //           onChange={(e) => console.log(e)}
+          // />
+          // <label className="btn btn-outline-primary" htmlFor="vol50">50</label>
+      //   <label className="btn btn-outline-primary" htmlFor="vol50">50</label>
+
+      //   <input type="radio" className="radio btn-check" name="vol" id="vol1000" checked={currentVolume === 1000} value="10"
+      //     //onClick={handleChangeVolume}
+      //     onChange={(e) => console.log(e)}
+      //   />
+      //   <label className="btn btn-outline-primary" htmlFor="vol1000">1000</label>
+      // </div>
+      */}
       <div className="btn-group col-4">
         <input type="text" className="form-control" id="filter" />
         <button type="button" className="btn btn-primary">Найти</button>
       </div>
-      {/* </form> */}
     </div>
   )
 }
