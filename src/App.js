08d1917ee93047bf45a765600ae17f925b2c6348
Peter Lebedev <LPeter@mail.ru>
2021-08-17 15:25:39 +0300

    move table to TableView.jsx

--- src/App.js
+++ src/App.js
@@ -1,46 +1,23 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import TableView from "./components/TableView";
 
 function App() {
   const BASE_URL = 'http://www.filltext.com/?rows=10&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone}&address={addressObject}&description={lorem|32}';
 
   // state for data
-  const [data, setData] = useState([]);
+  const [smallData, setSmallData] = useState([]);
 
   useEffect(() => {
     axios.get(BASE_URL).then(res => {
       console.log(res.data);
-      setData(res.data);
+      setSmallData(res.data);
     })
   }, [])
 
   return (
     <div className="container">
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">id</th>
-            <th scope="col">forstName</th>
-            <th scope="col">lastName</th>
-            <th scope="col">email</th>
-            <th scope="col">phone</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            data.map(item =>
-              <tr>
-                <th scope="row">{item.id}</th>
-                <td>{item.firstName}</td>
-                <td>{item.lastName}</td>
-                <td>{item.phone}</td>
-                <td>{item.email}</td>
-              </tr>
-            )
-          }
-        </tbody>
-      </table>
-
+      <TableView smallData={smallData} />
     </div>
   );
 }
---
+++ src/components/TableView.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function TableView(props) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">id</th>
+          <th scope="col">forstName</th>
+          <th scope="col">lastName</th>
+          <th scope="col">email</th>
+          <th scope="col">phone</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          props.smallData.map(item =>
+            <tr>
+              <th scope="row">{item.id}</th>
+              <td>{item.firstName}</td>
+              <td>{item.lastName}</td>
+              <td>{item.phone}</td>
+              <td>{item.email}</td>
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
+  );
+}
