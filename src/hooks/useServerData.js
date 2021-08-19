f9a102ae33da7eabe14a8b81fba9fe1b21a24fcb
Peter Lebedev <LPeter@mail.ru>
2021-08-19 17:25:17 +0300

    add format to phone field as (XXX)XXX-XXXX

--- src/hooks/useServerData.js
+++ src/hooks/useServerData.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export const useServerData = (volume) => {
   const [contactData, setContactData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const url = `http://www.filltext.com/?rows=${volume || 10}&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone}&address={addressObject}&description={lorem|32}`;
+  const url = `http://www.filltext.com/?rows=${volume || 10}&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(XXX)XXX-XXXX}&address={addressObject}&description={lorem|32}`;
 
   const getData = () => { }
   useEffect(() => {
